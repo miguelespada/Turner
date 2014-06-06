7690e8c5e379a19e4f4fdeb6aa17778f7071d136
miguel.valero.espada@gmail.com <miguel.valero.espada@gmail.com>
2014-06-06 14:20:56 +0200

    working on logic

---
+++ Assets/Scripts/logic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logic : MonoBehaviour {
+
+	// Use this for initialization
+	public int state = 0;
+	public GameObject cup;
+	public GameObject balon;
+	public GameObject camiseta;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void nextState(){
+		state += 1;
+		if(state == 1){
+			balon.active = false;
+			cup.active = true;
+		}
+		if(state == 2){
+			cup.active = false;
+			camiseta.active = true;
+		}
+		if(state == 3){
+			camiseta.active = false;
+		}
+	}
+}
