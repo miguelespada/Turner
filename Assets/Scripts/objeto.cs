28e01436136c121fdf0784f91e011269c98adb09
miguelespada <miguel.valero.espada@gmail.com>
2014-06-06 10:33:19 +0200

    add animaiton selector

--- Assets/Scripts/objeto.cs
+++ Assets/Scripts/objeto.cs
@@ -7,6 +7,20 @@ public class objeto: MonoBehaviour {
 	
 	public GameObject Fx;
 
+	void Start ()
+	{
+		animation.Play ();
+		UnityEngine.Random.Range (0, animation.GetClipCount ());
+	}
+
+	void Update(){
+		if (Input.GetKeyDown ("a")){
+			int v = UnityEngine.Random.Range (0, animation.GetClipCount ());
+			animation.Play("anim_" + v);
+			              
+		}
+	}
+
 	void OnTriggerEnter2D(Collider2D other) {
 		audio.Play();
 		Fx = Instantiate(Fx, transform.position, transform.rotation) as GameObject;
