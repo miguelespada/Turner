42f983a158419b06b45d3022c6306ac170db66d9
miguelespada <miguel.valero.espada@gmail.com>
2014-06-07 17:22:20 +0200

    add osc listener

--- Assets/Scripts/animar/animar.cs
+++ Assets/Scripts/animar/animar.cs
@@ -12,29 +12,29 @@ public class animar : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetKeyDown ("1") || valor == 0) {
+
+		if ( valor == 0 ) {
 			skeletonAnimation.state.SetAnimation (0, "idle", true);
 			skeletonAnimation.timeScale = 1;
 		}
-		if (Input.GetKeyDown ("2") || valor == 1) {
+		if ( valor == 1 ) {
 			skeletonAnimation.state.SetAnimation (0, "idle/anim0", false);
 			skeletonAnimation.state.AddAnimation (0, "anim0", true, 1f);
 			skeletonAnimation.timeScale = 1;
 		}
-		if (Input.GetKeyDown ("3") || valor == 2) {
+		if ( valor == 2 ) {
 			skeletonAnimation.state.SetAnimation (0, "anim1", true);
 			skeletonAnimation.timeScale = 1;
 		}
-		if (Input.GetKeyDown ("4") || valor == 3) {
+		if ( valor == 3 ) {
 			skeletonAnimation.state.SetAnimation (0, "anim2", true);
 
 			skeletonAnimation.timeScale = 1;
 		}
 
-		if (Input.GetKeyDown ("5") || valor == 4) {
+		if (valor == 4 ) {
 			skeletonAnimation.state.SetAnimation (0, "anim2", true);
 			skeletonAnimation.timeScale = 2;
-
 		}
 		if(valor >= 0) valor = -1; 
 	}
--- Assets/Scripts/animar/fx.cs
+++ Assets/Scripts/animar/fx.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using Spine;
 using System;
 
-public class camera_fx : MonoBehaviour {
-
+public class fx : MonoBehaviour {
+	
 	int valor = -1;
 	private GlowEffect glow;
 	public GameObject fondo_red;
@@ -12,59 +12,86 @@ public class camera_fx : MonoBehaviour {
 	public GameObject Audio_2;
 	public GameObject Audio_3;
 	public GameObject Audio_4;
-
+	
+	public GameObject red_c;
+	public GameObject green_c;
+	public GameObject sparks;
+	public GameObject fireworks;
+	
 	void Start () {
 		glow = GetComponent<GlowEffect>();
 	}
-
+	
 	void Update () {
-		if (Input.GetKeyDown ("1") || valor == 0) {
+		if ( valor == 0 ) {
 			glow.enabled = false;
 			fondo_red.SetActive(false);
 			fondo_green.SetActive(true);
 			Audio_2.audio.mute = true; 
 			Audio_3.audio.mute = true; 
 			Audio_4.audio.mute = true; 
+			red_c.SetActive(false);
+			green_c.SetActive(false);
+			sparks.SetActive(false);
+			fireworks.SetActive(false);
 		}
-		if (Input.GetKeyDown ("2") || valor == 1) {
+		if ( valor == 1 ) {
 			glow.enabled = false;
 			fondo_red.SetActive(false);
 			fondo_green.SetActive(true);
 			Audio_2.audio.mute = false; 
 			Audio_3.audio.mute = true; 
 			Audio_4.audio.mute = true; 
+			
+			red_c.SetActive(true);
+			green_c.SetActive(false);
+			sparks.SetActive(false);
+			fireworks.SetActive(false);
 		}
-		if (Input.GetKeyDown ("3") || valor == 2) {
+		if ( valor == 2 ) {
 			glow.enabled = false;
 			fondo_red.SetActive(false);
 			fondo_green.SetActive(true);
 			Audio_2.audio.mute = false; 
 			Audio_3.audio.mute = false; 
-			Audio_4.audio.mute = true; 
+			Audio_4.audio.mute = true;			
+			red_c.SetActive(true);
+			green_c.SetActive(true);
+			sparks.SetActive(true);
+			fireworks.SetActive(false);
 		}
-		if (Input.GetKeyDown ("4") || valor == 3) {
+		if ( valor == 3 ) {
 			glow.enabled = false;
 			fondo_red.SetActive(false);
 			fondo_green.SetActive(true);
 			Audio_2.audio.mute = false; 
 			Audio_3.audio.mute = false; 
-			Audio_4.audio.mute = true; 
+			Audio_4.audio.mute = true;		
+			red_c.SetActive(true);
+			green_c.SetActive(true);
+			sparks.SetActive(true);
+			fireworks.SetActive(false);
 		}
 		
-		if (Input.GetKeyDown ("5") || valor == 4) {
+		if ( valor == 4 ) {
 			glow.enabled = true;
 			fondo_red.SetActive(true);
 			fondo_green.SetActive(false);
 			Audio_2.audio.mute = false; 
 			Audio_3.audio.mute = false; 
-			Audio_4.audio.mute = false; 
+			Audio_4.audio.mute = false;
+			red_c.SetActive(true);
+			green_c.SetActive(true);
+			sparks.SetActive(true);
+			fireworks.SetActive(true);
 		}
-
+		
+		if(valor >= 0) valor = -1; 
+		
 		float v = UnityEngine.Random.Range(0.2f, 0.3f);
 		glow.glowTint = new Color(v,v,v,1);
-		if(valor >= 0) valor = -1; 
 	}
-
+	
 	void anima(int v){
 		valor = v;
 	}
